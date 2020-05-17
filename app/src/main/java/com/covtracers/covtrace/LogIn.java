5a191575892b327827e70d049ee18ff563ecc963
Ethan Misaghi <ethan@emisaghi.com>
2020-05-17 15:55:07 -0600

    - Added firebase authentication - Sign In is working - Register is working - User can stay signed in - Forgot Password is working

---
+++ app/src/main/java/com/covtracers/covtrace/LogIn.java
@@ -0,0 +1,125 @@
+package com.covtracers.covtrace;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class LogIn extends AppCompatActivity {
+
+    private static final String TAG = "EmailPassword";
+    EditText emailText, passwordText;
+    ImageView imageView;
+    TextView textView, forgotPassword, signUpButton;
+    Button logInButton;
+    FirebaseAuth fAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.passwordText);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+        forgotPassword = findViewById(R.id.forgotPassword);
+        signUpButton = findViewById(R.id.signUpButton);
+        logInButton = findViewById(R.id.logInButton);
+
+        // Initialize Firebase Auth
+        fAuth = FirebaseAuth.getInstance();
+
+        // Check if user is signed in (non-null) and go to the next activity.
+        if (fAuth.getCurrentUser() != null) {
+            startActivity(new Intent(getApplicationContext(), Map.class));
+            finish();
+        }
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Register.class));
+                finish();
+            }
+        });
+
+        forgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailText.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    emailText.setError("Please fill in email!");
+                    return;
+                }
+                fAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            emailText.setError("Email sent!");
+                            Log.d(TAG, "Email sent.");
+                        } else {
+                            emailText.setError("Email was not found!");
+                            Log.d(TAG, "Email was not found.");
+                        }
+                    }
+                });
+            }
+        });
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailText.getText().toString().trim();
+                String password = passwordText.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    emailText.setError("Please fill in email!");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    passwordText.setError("Please fill in password!");
+                    return;
+                }
+                // authenticate the user
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, go to the map
+                            Log.d(TAG, "signInWithEmail:success");
+                            Toast.makeText(LogIn.this, "Logged in successfully.", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            startActivity(new Intent(getApplicationContext(), Map.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            emailText.setError("signInWithEmail:failure " + task.getException());
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LogIn.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
+    }
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/covtracers/covtrace/Register.java
+++ app/src/main/java/com/covtracers/covtrace/Register.java
@@ -1,14 +1,116 @@
 package com.covtracers.covtrace;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Register extends AppCompatActivity {
+
+    private static final String TAG = "EmailPassword";
+    EditText emailText, passwordText, confirmPasswordText;
+    ImageView imageView;
+    TextView textView, signInButton;
+    Button registerButton;
+    FirebaseAuth fAuth;
+
+    public static boolean isValidPassword(final String password) {
+
+        Pattern pattern;
+        Matcher matcher;
+        final String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z]).{8,}";
+        pattern = Pattern.compile(PASSWORD_PATTERN);
+        matcher = pattern.matcher(password);
+
+        return matcher.matches();
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        emailText = findViewById(R.id.emailText);
+        passwordText = findViewById(R.id.passwordText);
+        confirmPasswordText = findViewById(R.id.confirmPasswordText);
+        imageView = findViewById(R.id.imageView);
+        textView = findViewById(R.id.textView);
+        signInButton = findViewById(R.id.signInButton);
+        registerButton = findViewById(R.id.registerButton);
+
+        // Initialize Firebase Auth
+        fAuth = FirebaseAuth.getInstance();
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LogIn.class));
+                finish();
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailText.getText().toString().trim();
+                String password = passwordText.getText().toString().trim();
+                String confirmPassword = confirmPasswordText.getText().toString().trim();
+                if (TextUtils.isEmpty(email)) {
+                    emailText.setError("Please fill in email!");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)) {
+                    passwordText.setError("Please fill in password!");
+                    return;
+                }
+                if (!password.matches(confirmPassword)) {
+                    confirmPasswordText.setError("Passwords don't match!");
+                    return;
+                }
+                if (!isValidPassword(password)) {
+                    passwordText.setError("Please ensure your password is at least 8 characters, contains an uppercase, a lower case and a number.");
+                    return;
+                }
+
+                // Register the user in firebase
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, go to the map
+                            Log.d(TAG, "createUserWithEmail:success");
+                            Toast.makeText(Register.this, "User Created.", Toast.LENGTH_SHORT).show();
+                            FirebaseUser user = fAuth.getCurrentUser();
+                            startActivity(new Intent(getApplicationContext(), Map.class));
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            emailText.setError("createUserWithEmail:failure " + task.getException());
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(Register.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
     }
 }
