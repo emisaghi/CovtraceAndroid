ee49d58fa3be2e5bc0849ecace80913a6003a3cb
Jamie Huang <jamie.huang@gmail.com>
2020-05-16 23:33:35 -0700

    button linking to register page

--- app/src/main/java/com/covtracers/covtrace/Register.java
+++ app/src/main/java/com/covtracers/covtrace/Register.java
@@ -3,12 +3,31 @@ package com.covtracers.covtrace;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class Register extends AppCompatActivity {
+public class Register extends AppCompatActivity implements View.OnClickListener {
+
+    Button bRegister;
+    Button bSignIn;
+    EditText etEmail, etPassword, etConfirmPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        etEmail = (EditText) findViewById(R.id.emailText);
+        etPassword = (EditText) findViewById(R.id.passwordText);
+        etConfirmPassword = (EditText) findViewById(R.id.confirmPasswordText);
+        bRegister = (Button) findViewById(R.id.registerButton);
+
+        bRegister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
